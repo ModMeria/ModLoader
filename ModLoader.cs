9994a4972302519e25d07593464cf39a18462417
Luka <33029337+ggkkaa@users.noreply.github.com>
2025-10-18 16:54:55 +0100

    feat(loader): implement mod loading

--- ModLoader.cs
+++ ModLoader.cs
@@ -1,4 +1,6 @@
-﻿using Allumeria;
+﻿using System.Reflection;
+using Allumeria;
+using ModAPI.Abstractions;
 using ModAPI.Core;
 
 namespace Loader;
@@ -6,6 +8,10 @@ namespace Loader;
 public class ModMeriaLoader : IExternalLoader
 {
     public static Version Version { get; } = new(0, 1, 0);
+    
+    private readonly string _modsDirectory = "mods";
+    private readonly ModApi _api = new();
+    
     public void Init()
     {
         Logger.Init("Initializing ModMeria...");
@@ -14,8 +20,70 @@ public class ModMeriaLoader : IExternalLoader
         Logger.Init($"Game version: {fullVersion}");
 
         Game.VERSION = fullVersion + $" with ModMeria {Version}";
+    }
 
-        ModApi api = new();
+    public void LoadMods()
+    {
+        if (!Directory.Exists(_modsDirectory)) // This is dead code since modloader is ALWAYS in modsDirectory, but I included it just in case
+        {
+            Directory.CreateDirectory(_modsDirectory);
+        }
+        
+        var modFolders = Directory.GetDirectories(_modsDirectory);
 
+        var modNames = modFolders.Select(Path.GetFileName).ToList();
+
+        if (modNames.Count == 0)
+        {
+            Console.WriteLine("No mods found.");
+            return;
+        }
+
+        Console.WriteLine($"Found {modNames.Count} mod{(modNames.Count == 1 ? "" : "s")}:");
+        for (var i = 0; i < modNames.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}) {modNames[i]}");
+        }
+        Console.WriteLine();
+        
+        foreach (var folder in modFolders)
+        {
+            var modName = Path.GetFileName(folder);
+            var modDllPath = Path.Combine(folder, "mod.dll");
+
+            if (!File.Exists(modDllPath))
+            {
+                Logger.Error($"Mod {modName} is invalid! No Mod.dll found!");
+                continue;
+            }
+
+            try
+            {
+                Logger.Info($"Loading {modName}...");
+                var assembly = Assembly.LoadFrom(modDllPath);
+
+                var modTypes = assembly.GetTypes()
+                    .Where(t => typeof(IMod).IsAssignableFrom(t) && !t.IsAbstract && !t.IsInterface)
+                    .ToList();
+                ;
+
+                if (modTypes.Count == 0)
+                {
+                    Logger.Error($"Mod {modName} is invalid! No IMod implementation found!");
+                    continue;
+                }
+
+                foreach (var type in modTypes)
+                {
+                    var mod = (IMod)Activator.CreateInstance(type)!;
+                    mod.Init(_api);
+                    Logger.Info($"Finished loading mod {modName} ({type.FullName})");
+                }
+            }
+            catch (Exception exception)
+            {
+                Logger.Error($"Failed to load mod {modName}: {exception.Message}");
+            }
+        }
     }
 }
